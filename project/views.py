4c5adbfb7af08a45d060d67c04608ab36996996f
a-woods1 <seiadmin@Aubries-MacBook-Pro.local>
2018-06-18 13:46:23 -0400

    Updates to blog page

--- project/views.py
+++ project/views.py
@@ -7,6 +7,10 @@ from app import app, pages
 def home():
     return render_template('index.html')
 
+@app.route('/story/')
+def story():
+    return render_template('story.html')
+
 @app.route('/team/')
 def team():
     return render_template('team.html')
@@ -14,6 +18,22 @@ def team():
 @app.route('/blog/')
 def blog():
     return render_template('blog.html')
+
+@app.route('/blog/week_1')
+def blog_week_1():
+    return render_template('blog_entries/week_1.html')
+
+@app.route('/blog/week_2')
+def blog_week_2():
+    return render_template('blog_entries/week_2.html')
+
+@app.route('/blog/week_3')
+def blog_week_3():
+    return render_template('blog_entries/week_3.html')
+
+@app.route('/blog/week_4')
+def blog_week_4():
+    return render_template('blog_entries/week_4.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
