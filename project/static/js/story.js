e44c36b3bf127fcba3849cfd783791b4d94db37c
Asha <toulmina@gmail.com>
2018-07-11 21:14:39 -0400

    Major updates to story page
    Updated most of content on Story Page

--- project/static/js/story.js
+++ project/static/js/story.js
@@ -1,3 +1,24 @@
+// Code to show different things people are weighing on whether or not to disclose
+
+var affirmations = ["If a person's disability is visible to others, they may have to disclose, while people with less visible" +
+" disabilities do not have to do so", 
+"If a person's disability prevents them from completing an essential task, they may have to disclose in order to move forward" +
+" with the hiring process", 
+"If a person feels a certain level of trust in the company or employer, they may be more likely to disclose", 
+"Some people may feel that their disability is an important part of their identity, while others may not, which can affect how and when they disclose", 
+"People who have grown up under the protections of the ADA may be more likely to disclose, versus those who have not always had this type of legal protection"]
+
+
+$(document).on('click','.ball-solid',function(){
+          var itemID = $(this).attr('id')
+        $(".empty-text").text(affirmations[itemID]);
+    });
+
+
+
+
+
+
 
 // Trigger CSS animations on scroll.
 // Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
